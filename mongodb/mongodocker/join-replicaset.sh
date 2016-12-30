c2fc818d77082164821387c3f8d7c73bde180d69
Kevin Krummenauer <kevin@whiledo.de>
2016-12-30 21:49:09 +0000

    replicaset mongo erweitert

--- mongodb/mongodocker/join-replicaset.sh
+++ mongodb/mongodocker/join-replicaset.sh
@@ -3,18 +3,17 @@
 #Ein paar Sekunden schlafen, da parallel erstmal die Mongo DB gestartet wird
 sleep 5
 
-#Lese bisherige Anzahl der Mongo-Instanzen
-#nslookup gibt die IP Adressen der Instanzen aus
-#sed entfernt alle Zeilen, die nicht mit Address anfangen
-#awk gibt die dritte Spalte aus, dort stehen die IP Adressen
-#wc -l zaehlt die Zeilen
-MONGO_INSTANZEN=$(nslookups tasks.$MONGO_DNS_NAME | sed '/^Address/!d' | awk '{print $3}')
-ANZAHL_MONGO_INSTANZEN=$(echo $MONGO_INSTANZEN | wc -l)
+#Lese die IP Adressen der bisherigen Mongo Instanzen
+MONGO_INSTANZEN=$(getent hosts tasks.$MONGO_DNS_NAME | awk '{ print $1 }')
+ANZAHL_MONGO_INSTANZEN=$(echo $MONGO_INSTANZEN | wc -w)
 
-if [[ ANZAHL_MONGO_INSTANZEN <= 1 ]] ; then
+echo "Mongo Instanzen ($ANZAHL_MONGO_INSTANZEN)"
+echo $MONGO_INSTANZEN
+
+if [[ $ANZAHL_MONGO_INSTANZEN -le 1 ]] ; then
    EIGENE_IP_ADRESSE=$(hostname --ip-address)
    mongo --eval "rs.initiate({'_id':'newspage-mongo-set','members':[{'_id':0,'host':'$EIGENE_IP_ADRESSE:27017'}]})"
-   echo "ReplicaSet initialisiert"
+   echo "ReplicaSet initialisiert auf $EIGENE_IP_ADRESSE"
 else
 
    ADRESSE_PRIMARY=""
@@ -22,10 +21,11 @@ else
    for i in $MONGO_INSTANZEN; do
       #Frage auf allen Mongo Instanzen nach der Primary
       ADRESSE_PRIMARY=$(mongo $i --quiet --eval "rs.isMaster()['primary']")
-      
+      echo "$i nach Primary gefragt"
+
       if [[ ! -z $ADRESSE_PRIMARY ]] ; then
          break
-      if
+      fi
    done
    
    if [[ -z $ADRESSE_PRIMARY ]] ; then
