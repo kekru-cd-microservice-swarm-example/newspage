8f0f1e02f6015037a4acf3be4f295feb2026a3f8
Kevin Krummenauer <kevin.krummenauer@adesso.de>
2016-12-30 17:42:00 +0100

    Leeres Spring Boot Projekt und Dockerfile für Mongo DB

---
+++ src/main/java/de/fh_dortmund/kekru001/bachelorarbeit/newspage/NewspageApplication.java
@@ -0,0 +1,12 @@
+package de.fh_dortmund.kekru001.bachelorarbeit.newspage;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NewspageApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NewspageApplication.class, args);
+	}
+}
---
+++ src/test/java/de/fh_dortmund/kekru001/bachelorarbeit/newspage/NewspageApplicationTests.java
@@ -0,0 +1,16 @@
+package de.fh_dortmund.kekru001.bachelorarbeit.newspage;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class NewspageApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
