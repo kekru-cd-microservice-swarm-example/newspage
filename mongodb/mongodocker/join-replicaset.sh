99ceb4bfad89eb4ad514b09e089f65424c1cd081
Kevin Krummenauer <kevin@whiledo.de>
2016-12-31 01:05:15 +0000

    replicaset läuft jetzt. Master wir in Redis gespeichert

--- mongodb/mongodocker/join-replicaset.sh
+++ mongodb/mongodocker/join-replicaset.sh
@@ -1,40 +1,19 @@
 #!/bin/bash
 
-#Ein paar Sekunden schlafen, da parallel erstmal die Mongo DB gestartet wird
 sleep 5
 
-#Lese die IP Adressen der bisherigen Mongo Instanzen
-MONGO_INSTANZEN=$(getent hosts tasks.$MONGO_DNS_NAME | awk '{ print $1 }')
-ANZAHL_MONGO_INSTANZEN=$(echo $MONGO_INSTANZEN | wc -w)
+EIGENE_IP_ADRESSE=$(hostname --ip-address)
 
-echo "Mongo Instanzen ($ANZAHL_MONGO_INSTANZEN)"
-echo $MONGO_INSTANZEN
+if [[ $(curl -s 'http://webdis:7379/EXISTS/newspage-mongo-primary' | jq -r '.EXISTS') == 0 ]]; then
 
-if [[ $ANZAHL_MONGO_INSTANZEN -le 1 ]] ; then
-   EIGENE_IP_ADRESSE=$(hostname --ip-address)
+   curl -s "http://webdis:7379/SET/newspage-mongo-primary/$EIGENE_IP_ADRESSE"
    mongo --eval "rs.initiate({'_id':'newspage-mongo-set','members':[{'_id':0,'host':'$EIGENE_IP_ADRESSE:27017'}]})"
-   echo "ReplicaSet initialisiert auf $EIGENE_IP_ADRESSE"
+   echo "ReplicaSet initialisiert auf $EIGENE_IP_ADRESSE"   
+
 else
-
-   ADRESSE_PRIMARY=""
    
-   for i in $MONGO_INSTANZEN; do
-      #Frage auf allen Mongo Instanzen nach der Primary
-      ADRESSE_PRIMARY=$(mongo $i --quiet --eval "rs.isMaster()['primary']")
-      echo "$i nach Primary gefragt"
+   MONGO_PRIMARY=$(curl -s 'http://webdis:7379/GET/newspage-mongo-primary' | jq -r '.GET')
+   mongo "$MONGO_PRIMARY:27017" --eval "rs.add('$EIGENE_IP_ADRESSE:27017')"
+   echo "Registriert (Eigene IP: $EIGENE_IP_ADRESSE) an Primary: $MONGO_PRIMARY"
 
-      if [[ ! -z $ADRESSE_PRIMARY ]] ; then
-         break
-      fi
-   done
-   
-   if [[ -z $ADRESSE_PRIMARY ]] ; then
-       echo "Primary Adresse nicht gefunden"
-       exit 1
-   fi
-    
-   echo "Primary ist $ADRESSE_PRIMARY"
 fi
-
-# Starte den urspruenglichen entrypoint mit den Parametern, die das aktuelle Script erhalten hat
-/entrypoint.sh "$@"
