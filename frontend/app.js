06400ef2a6dafa63875dc21c66259f9bab0a9251
Kevin Krummenauer <kevin.krummenauer@adesso.de>
2017-01-18 20:23:08 +0100

    locationprovider html5 herausgenommen

--- frontend/app.js
+++ frontend/app.js
@@ -1,5 +1,5 @@
-//var baseURL = 'http://localhost:8081/api/';
-var baseURL = 'api/';
+var baseURL = 'http://localhost:8081/api/';
+//var baseURL = '/api/';
 //var baseURL = 'http://10.1.6.210:30000/api/';
 
 angular.module('NewsApp', []);
@@ -20,7 +20,6 @@ angular.module('NewsApp', ['ngRoute'])
                     redirectTo: '/'
                 });
 
-            $locationProvider.html5Mode(true);
         }])
 
 
