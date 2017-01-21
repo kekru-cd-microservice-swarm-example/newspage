81bf89d9af5ffe54044ac1ceca7fd235fc7772d5
Kevin Krummenauer <kevin.krummenauer@adesso.de>
2017-01-21 21:08:37 +0100

    Mongo DB spricht direkt mit Redis und nicht mehr über Webdis

--- mongodb/mongodocker/entrypoint-initreplicas.sh
+++ mongodb/mongodocker/entrypoint-initreplicas.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
+# In separatem Thread dem Replicaset beitreten
 /data/join-replicaset.sh &
 
 # Starte den urspruenglichen entrypoint mit den Parametern, die das aktuelle Script erhalten hat
--- mongodb/mongodocker/join-replicaset.sh
+++ mongodb/mongodocker/join-replicaset.sh
@@ -1,18 +1,20 @@
 #!/bin/bash
 
-sleep 15
+#15 bis 25 Sekunden schlafen
+SLEEPTIME=$(( ($RANDOM %10) + 15))
+echo "Sleep $SLEEPTIME seconds"
+sleep $SLEEPTIME
 
 EIGENE_IP_ADRESSE=$(hostname --ip-address)
 
-if [[ $(curl -s 'http://webdis:7379/EXISTS/newspage-mongo-primary' | jq -r '.EXISTS') == 0 ]]; then
+MONGO_PRIMARY=$(/data/redi.sh -g newspage-mongo-primary) 2>/dev/null
 
-   curl -s "http://webdis:7379/SET/newspage-mongo-primary/$EIGENE_IP_ADRESSE"
+if [[ -z $MONGO_PRIMARY ]]; then
+   echo "$EIGENE_IP_ADRESSE" | /data/redi.sh -s newspage-mongo-primary
    mongo --eval "rs.initiate({'_id':'newspage-mongo-set','members':[{'_id':0,'host':'$EIGENE_IP_ADRESSE:27017'}]})"
    echo "ReplicaSet initialisiert auf $EIGENE_IP_ADRESSE"   
 
 else
-   
-   MONGO_PRIMARY=$(curl -s 'http://webdis:7379/GET/newspage-mongo-primary' | jq -r '.GET')
    mongo "$MONGO_PRIMARY:27017" --eval "rs.add('$EIGENE_IP_ADRESSE:27017')"
    echo "Registriert (Eigene IP: $EIGENE_IP_ADRESSE) an Primary: $MONGO_PRIMARY"
 
